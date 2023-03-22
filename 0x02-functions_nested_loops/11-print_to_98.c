2229f5eadd7a51bc2e28af22b47e42f905c76f19
Weliia <ayaelouahabi818@gmail.com>
2023-03-22 14:57:36 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 #include "main.h"
 
 /**
@@ -20,3 +20,13 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
+}
