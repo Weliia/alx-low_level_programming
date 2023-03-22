d272a466ccd96a375d7ebcfeb9d4b0e5c300ba83
Weliia <ayaelouahabi818@gmail.com>
2023-03-22 14:34:52 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * times_table - prints the 9 times table, starting with 0
  */
@@ -24,4 +25,12 @@ void times_table(void)
 				_putchar(k + '0');
 			} else if (k >= 10)
 			{
-			  
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
